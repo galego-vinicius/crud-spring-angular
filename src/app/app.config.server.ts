b1f4213ce773f51e5cd10610a368b8a716146f32
galego-vinicius <vfelix1998@gmail.com>
2024-06-11 14:11:56 -0300

    add: ToolBar + Criacao de Modulos + Roteamento Lazy Loading

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,10 +1,11 @@
-import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
+import { mergeApplicationConfig, ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 const serverConfig: ApplicationConfig = {
   providers: [
-    provideServerRendering()
+    provideServerRendering(), importProvidersFrom(MatToolbarModule)
   ]
 };
 
