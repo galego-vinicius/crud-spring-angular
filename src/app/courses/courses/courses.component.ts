8585b60720c2120b8c6cc060e788b9cac6aa18fb
galego-vinicius <vfelix1998@gmail.com>
2024-06-17 13:55:54 -0300

    add: criando material table para listar cursos

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { Course } from '../models/course';
+import {MatTableModule} from '@angular/material/table';
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
 export class CoursesComponent {
-
+  courses: Course[] = [
+    { _id: '1', name: 'Angular', category: 'Front-end'}
+  ] //Criando construtor;
+  displayedColumns = ['name', 'category'];
 }
