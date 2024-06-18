b0104a39d6f7db0f4865cf4c3f664dfba10ff39c
galego-vinicius <vfelix1998@gmail.com>
2024-06-18 11:41:01 -0300

    add: Lista de cursos - Tratamento de erros + MatDialog + Style

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -4,15 +4,18 @@ import { Course } from '../models/course';
 import {MatTableModule} from '@angular/material/table';
 import {MatCardModule} from '@angular/material/card';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import { CommonModule } from '@angular/common';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from '../../shared/components/error-dialog/error-dialog.component';
 
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [MatTableModule, MatCardModule, MatToolbarModule, MatProgressSpinnerModule, CommonModule],
+  imports: [MatTableModule, MatCardModule, MatToolbarModule, MatProgressSpinnerModule,
+     CommonModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
@@ -20,7 +23,19 @@ export class CoursesComponent {
   courses$: Observable <Course[]>;
   displayedColumns = ['name', 'category'];
 
-  constructor(private coursesService: CourseService){
-    this.courses$ = this.coursesService.list();
+  constructor(private coursesService: CourseService, public dialog: MatDialog){
+    this.courses$ = this.coursesService.list()
+    .pipe(
+      catchError(error => {
+        this.onError('Erro ao carregar cursos!')
+        return of([]) // Cria um observable com array vazio.
+      })
+    );
+  }
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
   }
 }
